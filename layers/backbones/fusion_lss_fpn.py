24fe01b4d7aa484a7c18baf8fee444a675632659
Richard Hendricks <liyinhao0413@qq.com>
2022-09-29 10:45:14 +0800

    Use lidar_depth conv.

--- layers/backbones/fusion_lss_fpn.py
+++ layers/backbones/fusion_lss_fpn.py
@@ -1,23 +1,27 @@
 # Copyright (c) Megvii Inc. All rights reserved.
 import torch
-from mmdet3d.models import build_neck
-from mmdet.models import build_backbone
+import torch.nn as nn
 
 from ops.voxel_pooling import voxel_pooling
 
-from .base_lss_fpn import BaseLSSFPN, DepthNet
+from .base_lss_fpn import BaseLSSFPN
+from .base_lss_fpn import DepthNet as BaseDepthNet
 
 __all__ = ['FusionLSSFPN']
 
 
-class ContextNet(DepthNet):
+class DepthNet(BaseDepthNet):
     def __init__(self, in_channels, mid_channels, context_channels,
                  depth_channels):
-        super(ContextNet, self).__init__(in_channels, mid_channels,
-                                         context_channels, depth_channels)
-        del self.depth_conv
+        super(DepthNet, self).__init__(in_channels, mid_channels,
+                                       context_channels, depth_channels)
+        self.lidar_depth_conv = nn.Sequential(
+            nn.Conv2d(1, mid_channels, kernel_size=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, mid_channels, kernel_size=1, stride=1),
+        )
 
-    def forward(self, x, mats_dict):
+    def forward(self, x, mats_dict, lidar_depth):
         intrins = mats_dict['intrin_mats'][:, 0:1, ..., :3, :3]
         batch_size = intrins.shape[0]
         num_cams = intrins.shape[2]
@@ -56,125 +60,24 @@ class ContextNet(DepthNet):
         context_se = self.context_mlp(mlp_input)[..., None, None]
         context = self.context_se(x, context_se)
         context = self.context_conv(context)
-        return context
+        depth_se = self.depth_mlp(mlp_input)[..., None, None]
+        depth = self.lidar_depth_conv(lidar_depth)
+        depth = self.depth_se(x + depth, depth_se)
+        depth = self.depth_conv(depth)
+        return torch.cat([depth, context], dim=1)
 
 
 class FusionLSSFPN(BaseLSSFPN):
-    def __init__(self, x_bound, y_bound, z_bound, d_bound, final_dim,
-                 downsample_factor, output_channels, img_backbone_conf,
-                 img_neck_conf, depth_net_conf):
-        """Modified from `https://github.com/nv-tlabs/lift-splat-shoot`.
-
-        Args:
-            x_bound (list): Boundaries for x.
-            y_bound (list): Boundaries for y.
-            z_bound (list): Boundaries for z.
-            d_bound (list): Boundaries for d.
-            final_dim (list): Dimension for input images.
-            downsample_factor (int): Downsample factor between feature map
-                and input image.
-            output_channels (int): Number of channels for the output
-                feature map.
-            img_backbone_conf (dict): Config for image backbone.
-            img_neck_conf (dict): Config for image neck.
-            depth_net_conf (dict): Config for depth net.
-        """
-
-        super(BaseLSSFPN, self).__init__()
-        self.downsample_factor = downsample_factor
-        self.d_bound = d_bound
-        self.final_dim = final_dim
-        self.output_channels = output_channels
-
-        self.register_buffer(
-            'voxel_size',
-            torch.Tensor([row[2] for row in [x_bound, y_bound, z_bound]]))
-        self.register_buffer(
-            'voxel_coord',
-            torch.Tensor([
-                row[0] + row[2] / 2.0 for row in [x_bound, y_bound, z_bound]
-            ]))
-        self.register_buffer(
-            'voxel_num',
-            torch.LongTensor([(row[1] - row[0]) / row[2]
-                              for row in [x_bound, y_bound, z_bound]]))
-        self.register_buffer('frustum', self.create_frustum())
-        self.depth_channels, _, _, _ = self.frustum.shape
-
-        self.img_backbone = build_backbone(img_backbone_conf)
-        self.img_neck = build_neck(img_neck_conf)
-        self.context_net = self._configure_context_net(depth_net_conf)
-
-        self.img_neck.init_weights()
-        self.img_backbone.init_weights()
-
-    def _configure_context_net(self, depth_net_conf):
-        return ContextNet(
+    def _configure_depth_net(self, depth_net_conf):
+        return DepthNet(
             depth_net_conf['in_channels'],
             depth_net_conf['mid_channels'],
             self.output_channels,
             self.depth_channels,
         )
 
-    def _forward_context_net(self, feat, mats_dict):
-        return self.context_net(feat, mats_dict)
-
-    def get_geometry(self, depth, sensor2ego_mat, intrin_mat, ida_mat,
-                     bda_mat):
-        """Transfer points from camera coord to ego coord.
-
-        Args:
-            rots(Tensor): Rotation matrix from camera to ego.
-            trans(Tensor): Translation matrix from camera to ego.
-            intrins(Tensor): Intrinsic matrix.
-            post_rots_ida(Tensor): Rotation matrix for ida.
-            post_trans_ida(Tensor): Translation matrix for ida
-            post_rot_bda(Tensor): Rotation matrix for bda.
-
-        Returns:
-            Tensors: points ego coord.
-        """
-        device = depth.device
-        batch_size_with_num_cams = depth.shape[0]
-        ogfH, ogfW = self.final_dim
-        fH, fW = ogfH // self.downsample_factor, ogfW // self.downsample_factor
-        x_coords = torch.linspace(0,
-                                  ogfW - 1,
-                                  fW,
-                                  dtype=torch.float,
-                                  device=device).view(1, 1, 1, fW).expand(
-                                      batch_size_with_num_cams, 1, fH, fW)
-        y_coords = torch.linspace(0,
-                                  ogfH - 1,
-                                  fH,
-                                  dtype=torch.float,
-                                  device=device).view(1, 1, fH, 1).expand(
-                                      batch_size_with_num_cams, 1, fH, fW)
-        paddings = torch.ones_like(depth)
-
-        # D x H x W x 3
-        xyz_coords = torch.stack((x_coords, y_coords, depth, paddings), -1)
-        batch_size, num_cams, _, _ = sensor2ego_mat.shape
-        points = xyz_coords.reshape(batch_size, num_cams,
-                                    *xyz_coords.shape[1:])
-        # undo post-transformation
-        ida_mat = ida_mat.view(batch_size, num_cams, 1, 1, 1, 4, 4)
-        points = ida_mat.inverse().matmul(points.unsqueeze(-1))
-        # cam_to_ego
-        points = torch.cat(
-            (points[:, :, :, :, :, :2] * points[:, :, :, :, :, 2:3],
-             points[:, :, :, :, :, 2:]), 5)
-
-        combine = sensor2ego_mat.matmul(torch.inverse(intrin_mat))
-        points = combine.view(batch_size, num_cams, 1, 1, 1, 4,
-                              4).matmul(points)
-        if bda_mat is not None:
-            bda_mat = bda_mat.unsqueeze(1).repeat(1, num_cams, 1, 1).view(
-                batch_size, num_cams, 1, 1, 1, 4, 4)
-            points = (bda_mat @ points).squeeze(-1)
-        else:
-            points = points.squeeze(-1)
-        return points[..., :3]
+    def _forward_depth_net(self, feat, mats_dict, lidar_depth):
+        return self.depth_net(feat, mats_dict, lidar_depth)
 
     def _forward_single_sweep(self,
                               sweep_index,
@@ -209,16 +112,19 @@ class FusionLSSFPN(BaseLSSFPN):
         batch_size, num_sweeps, num_cams, num_channels, img_height, \
             img_width = sweep_imgs.shape
         img_feats = self.get_cam_feats(sweep_imgs)
+        lidar_depth = lidar_depth.reshape(batch_size * num_cams,
+                                          *lidar_depth.shape[2:])
         source_features = img_feats[:, 0, ...]
-        context = self._forward_context_net(
+        depth_feature = self._forward_depth_net(
             source_features.reshape(batch_size * num_cams,
                                     source_features.shape[2],
                                     source_features.shape[3],
-                                    source_features.shape[4]),
-            mats_dict,
-        )
-        depth = self.get_downsampled_lidar_depth(lidar_depth.squeeze(1))
-        img_feat_with_depth = context.unsqueeze(2)
+                                    source_features.shape[4]), mats_dict,
+            lidar_depth)
+        depth = depth_feature[:, :self.depth_channels].softmax(1)
+        img_feat_with_depth = depth.unsqueeze(
+            1) * depth_feature[:, self.depth_channels:(
+                self.depth_channels + self.output_channels)].unsqueeze(2)
 
         img_feat_with_depth = self._forward_voxel_net(img_feat_with_depth)
 
@@ -231,7 +137,6 @@ class FusionLSSFPN(BaseLSSFPN):
             img_feat_with_depth.shape[4],
         )
         geom_xyz = self.get_geometry(
-            depth,
             mats_dict['sensor2ego_mats'][:, sweep_index, ...],
             mats_dict['intrin_mats'][:, sweep_index, ...],
             mats_dict['ida_mats'][:, sweep_index, ...],
@@ -240,9 +145,10 @@ class FusionLSSFPN(BaseLSSFPN):
         img_feat_with_depth = img_feat_with_depth.permute(0, 1, 3, 4, 5, 2)
         geom_xyz = ((geom_xyz - (self.voxel_coord - self.voxel_size / 2.0)) /
                     self.voxel_size).int()
-        feature_map = voxel_pooling(geom_xyz,
-                                    img_feat_with_depth.contiguous().float(),
+        feature_map = voxel_pooling(geom_xyz, img_feat_with_depth.contiguous(),
                                     self.voxel_num.cuda())
+        if is_return_depth:
+            return feature_map.contiguous(), depth
         return feature_map.contiguous()
 
     def forward(self,
@@ -277,12 +183,12 @@ class FusionLSSFPN(BaseLSSFPN):
         """
         batch_size, num_sweeps, num_cams, num_channels, img_height, \
             img_width = sweep_imgs.shape
-
+        lidar_depth = self.get_downsampled_lidar_depth(lidar_depth)
         key_frame_res = self._forward_single_sweep(
             0,
             sweep_imgs[:, 0:1, ...],
             mats_dict,
-            lidar_depth[:, 0:1, ...],
+            lidar_depth[:, 0, ...],
             is_return_depth=is_return_depth)
         if num_sweeps == 1:
             return key_frame_res
@@ -295,9 +201,9 @@ class FusionLSSFPN(BaseLSSFPN):
             with torch.no_grad():
                 feature_map = self._forward_single_sweep(
                     sweep_index,
-                    sweep_imgs[:, sweep_index:sweep_index + 1, ...],
+                    sweep_imgs[:, sweep_index, ...],
                     mats_dict,
-                    lidar_depth[:, sweep_index:sweep_index + 1, ...],
+                    lidar_depth[:, sweep_index, ...],
                     is_return_depth=False)
                 ret_feature_list.append(feature_map)
 
@@ -307,29 +213,23 @@ class FusionLSSFPN(BaseLSSFPN):
             return torch.cat(ret_feature_list, 1)
 
     def get_downsampled_lidar_depth(self, lidar_depth):
-        """
-        Input:
-            gt_depths: [B, N, H, W]
-        Output:
-            gt_depths: [B*N*h*w, d]
-        """
-        B, N, H, W = lidar_depth.shape
+        batch_size, num_sweeps, num_cams, height, width = lidar_depth.shape
         lidar_depth = lidar_depth.view(
-            B * N,
-            H // self.downsample_factor,
+            batch_size * num_sweeps * num_cams,
+            height // self.downsample_factor,
             self.downsample_factor,
-            W // self.downsample_factor,
+            width // self.downsample_factor,
             self.downsample_factor,
             1,
         )
         lidar_depth = lidar_depth.permute(0, 1, 3, 5, 2, 4).contiguous()
         lidar_depth = lidar_depth.view(
             -1, self.downsample_factor * self.downsample_factor)
-        gt_depths_tmp = torch.where(
-            lidar_depth == 0.0, self.d_bound[1] * torch.ones_like(lidar_depth),
-            lidar_depth)
+        gt_depths_tmp = torch.where(lidar_depth == 0.0, lidar_depth.max(),
+                                    lidar_depth)
         lidar_depth = torch.min(gt_depths_tmp, dim=-1).values
-        lidar_depth = lidar_depth.view(B * N, H // self.downsample_factor,
-                                       W // self.downsample_factor)
-
-        return lidar_depth.unsqueeze(1)
+        lidar_depth = lidar_depth.view(batch_size, num_sweeps, num_cams, 1,
+                                       height // self.downsample_factor,
+                                       width // self.downsample_factor)
+        lidar_depth = lidar_depth / self.d_bound[1]
+        return lidar_depth
